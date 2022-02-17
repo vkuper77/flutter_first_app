86187056dd0eaf8b8e0ebe3a905458e634f0523a
Vitali <kupratsevich@msa-it.ru>
2022-02-17 23:16:52 +0300

    method onPressed

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestions() {
+    questionIndex = questionIndex + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s you favorite color?',
+      'What\'s you favorite animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default test!'),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              onPressed: answerQuestions,
+              child: Text('Answer 1'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestions,
+              child: Text('Answer 2'),
+            ),
+            RaisedButton(
+              onPressed: answerQuestions,
+              child: Text('Answer 3'),
+            )
+          ],
+        ),
       ),
     );
   }
