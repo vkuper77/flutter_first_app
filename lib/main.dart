d8ccf021718355e255fd3e1d021fd5bbedb67439
Vitali <kupratsevich@msa-it.ru>
2022-03-11 00:14:02 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -18,30 +18,30 @@ class _MyAppState extends State<MyApp> {
     {
       'questionText': 'What\'s you favorite color?',
       'answers': [
-        {'text': 'Red', 'score': 10},
-        {'text': 'Blue', 'score': 30},
-        {'text': 'Green', 'score': 5},
-        {'text': 'White', 'score': 70},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Black', 'score': 10},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
       ]
     },
     {
       'questionText': 'What\'s you favorite animal?',
       'answers': [
         {'text': 'Cat', 'score': 4},
-        {'text': 'Dog', 'score': 7},
+        {'text': 'Dog', 'score': 6},
         {'text': 'Rabbit', 'score': 2},
-        {'text': 'Snake', 'score': 8},
-        {'text': 'Lion', 'score': 3},
+        {'text': 'Snake', 'score': 10},
+        {'text': 'Lion', 'score': 7},
         {'text': 'Elephant', 'score': 5}
       ]
     },
     {
       'questionText': 'What\'s you favorite dish?',
       'answers': [
-        {'text': 'Pizza', 'score': 3},
-        {'text': 'Burger', 'score': 7},
-        {'text': 'Sushi', 'score': 9},
-        {'text': 'potate', 'score': 5}
+        {'text': 'Pizza', 'score': 4},
+        {'text': 'Burger', 'score': 6},
+        {'text': 'Sushi', 'score': 2},
+        {'text': 'potate', 'score': 3}
       ]
     },
   ];
